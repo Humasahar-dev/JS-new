a7b068b1fee3b7a140809c2c63e6646b626ae1de
huma sahar <humasahar66@gmail.com>
2023-04-07 02:02:24 +0500

    FEAT:JS Intro Class

---
+++ scripts/app.js
@@ -0,0 +1,33 @@
+// let name="Ishaq";
+// alert(name); 
+// alert("hello word");
+// prompt("your name please"," sahar baig")
+// var user = prompt("your name please"," sahar baig");
+// var user = prompt( "your name please"," sahar baig ");
+// alert("user");
+
+// document.write(arr)
+// var arr = ['hello','h1'];
+// console.log('arr')
+// let data = " hello students ";
+// let data2 = "   pakistan"
+// let data3 = data + data2 ;
+// console.log(data3);
+
+// let data = " hello students ";
+// let data2 = 3  
+// let data3 = data + data2 ;
+// console.log(data3);
+
+// let data= "hello";
+// let data2 =data.includes("3");
+// console.log(data2);
+
+// let data= "5";
+// let data2= 5;
+// console.log (Number(data)+data2);
+
+// let data= "25";
+// let data2= 5;
+// console.log (data%data2);
+
